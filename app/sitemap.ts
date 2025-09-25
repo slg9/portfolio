eeba3e5fb71946a92f8e07954c224bac7cf8983d
slg9 <slegros9@gmail.com>
2025-09-25 09:49:00 +0400

    update sitempa

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/#techno`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/#cursus`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/#contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
